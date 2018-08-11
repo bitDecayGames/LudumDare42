9384588284aa8fd36c1a687a9facfe5e032a8f6c
mwingfield <mwingfie@gmail.com>
2018-08-11 12:38:30 -0600

    spawn animation on tele ball destroyed

---
+++ Assets/Scripts/Player/SpawnOnDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Player {
+	public class SpawnOnDestroy : MonoBehaviour {
+		[Serializable]
+		public class OnSpawnEvent : UnityEvent<Transform> {}
+	
+		public Transform Spawn;
+		public OnSpawnEvent OnSpawn;
+	
+		private void OnDestroy() {
+			var spawn = Instantiate(Spawn);
+			spawn.position = transform.position;
+			spawn.rotation = transform.rotation;
+			OnSpawn.Invoke(spawn);
+		}
+	}
+}
