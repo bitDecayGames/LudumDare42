4f85de5045e7ac8eb4db6b2fd1edaa063197899b
mwingfield <mwingfie@gmail.com>
2018-08-11 00:00:04 -0600

    inverse gravity environment stuff

---
+++ Assets/Scripts/Environments/InverseGravityEnvironment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Environments {
+	public class InverseGravityEnvironment : TriggerEnvironment {
+
+		protected override void ApplyEffect(Collider2D thing) {
+			thing.attachedRigidbody.gravityScale = Math.Abs(thing.attachedRigidbody.gravityScale) * -1;
+		}
+
+		protected override void RemoveEffect(Collider2D thing) {
+			thing.attachedRigidbody.gravityScale = Math.Abs(thing.attachedRigidbody.gravityScale);
+		}
+	}
+}
---
+++ Assets/Scripts/Environments/TriggerEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environments {
+	public abstract class TriggerEnvironment : MonoBehaviour {
+
+		private List<Collider2D> actives = new List<Collider2D>();
+		
+		private void OnTriggerEnter2D(Collider2D other) {
+			ApplyEffect(other);
+		}
+		
+		private void OnTriggerExit2D(Collider2D other) {
+			RemoveEffect(other);
+		}
+		
+		protected abstract void ApplyEffect(Collider2D thing);
+		protected abstract void RemoveEffect(Collider2D thing);
+	}
+}
