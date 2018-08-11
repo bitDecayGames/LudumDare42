495703d66cc0a12c9acff321db15aabb97978a08
mwingfield <mwingfie@gmail.com>
2018-08-11 10:27:30 -0600

    follow rotation script for cam

---
+++ Assets/Scripts/Camera/FollowRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class FollowRotation : MonoBehaviour {
+    public Transform Follow;
+
+    void Update() {
+        transform.rotation.Set(Follow.rotation.x, Follow.rotation.y, Follow.rotation.z, Follow.rotation.w);
+    }
+}
