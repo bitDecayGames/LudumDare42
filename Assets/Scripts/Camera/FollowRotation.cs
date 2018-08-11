6a78a875a7ea736ba2ed7039aec0ba04770829c3
mwingfield <mwingfie@gmail.com>
2018-08-11 11:15:27 -0600

    sweet sweet camera smoothing

--- Assets/Scripts/Camera/FollowRotation.cs
+++ Assets/Scripts/Camera/FollowRotation.cs
@@ -3,7 +3,16 @@
 public class FollowRotation : MonoBehaviour {
     public Transform Follow;
 
+    [Range(0.0001f, 1)]
+    public float smoothing;
+
     void Update() {
-        transform.rotation = Quaternion.Euler(0, 0, Follow.rotation.eulerAngles.z);
+        var cur = transform.rotation.eulerAngles.z;
+        var target = Follow.rotation.eulerAngles.z;
+        var diff = target - cur;
+        if (diff > 180) diff -= 360;
+        else if (diff < -180) diff += 360;
+        var smoothed = diff * smoothing + cur;
+        transform.rotation = Quaternion.Euler(0, 0, smoothed);
     }
 }
