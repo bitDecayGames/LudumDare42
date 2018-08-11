7849484d4ab1823a9742b9a824231c8745f7338d
mwingfield <mwingfie@gmail.com>
2018-08-11 10:15:51 -0600

    water environment

---
+++ Assets/Scripts/Environments/WaterEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Environments {
+	public class WaterEnvironment : TriggerEnvironment {
+
+		protected override void ApplyEffect(Collider2D thing) {
+			thing.attachedRigidbody.drag = 4;
+		}
+
+		protected override void RemoveEffect(Collider2D thing) {
+			thing.attachedRigidbody.drag = 0;
+		}
+	}
+}
