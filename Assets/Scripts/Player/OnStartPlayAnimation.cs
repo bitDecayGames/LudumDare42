59ccd4aabc4a01d3b1fd8b64dd14d6c7f436452a
mwingfield <mwingfie@gmail.com>
2018-08-12 00:44:59 -0600

    add animations for exploding ball

---
+++ Assets/Scripts/Player/OnStartPlayAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Player {
+	[RequireComponent(typeof(Animator))]
+	public class OnStartPlayAnimation : MonoBehaviour {
+
+		public string AnimationName;
+
+		void Awake() {
+			Init();
+		}
+
+		private void Init() {
+			if (AnimationName != null) {
+				Debug.Log("Awake: " + AnimationName);
+				GetComponent<Animator>().Play(AnimationName);
+			}
+		}
+	}
+}
