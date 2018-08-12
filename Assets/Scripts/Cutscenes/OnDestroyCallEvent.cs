64b0e94afd6ad71442f1adeb6ee7cac97e81c8d7
Jacob Caban-Tomski <jacque006@gmail.com>
2018-08-12 16:40:08 -0600

    Trigger cutscene and spawn black hole

---
+++ Assets/Scripts/Cutscenes/OnDestroyCallEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+using System;
+
+public class OnDestroyCallEvent : MonoBehaviour {
+    [Serializable]
+    public class OnDestroyEvent : UnityEvent<Transform> {}
+		
+    public OnDestroyEvent OnDestroyed = new OnDestroyEvent();
+
+    void OnDestroy() {
+        OnDestroyed.Invoke(transform);
+    }
+}
--- Assets/Scripts/Cutscenes/StartCutscene.cs
+++ Assets/Scripts/Cutscenes/StartCutscene.cs
@@ -4,9 +4,25 @@ public class StartCutscene : MonoBehaviour {
 
     public Canvas CutscenePrefab;
 
+    public BlackHoleController BlackHolePrefab;
+
+    public GameObject PathMarkers;
+
+    public BrownHoleEffect ForegroundCamera;
+
+    public Transform BlackHoleSpawnPoint;
+
     public void OnStartCutscene() {
         var cutscene = Instantiate(CutscenePrefab);
+        cutscene.GetComponent<OnDestroyCallEvent>().OnDestroyed.AddListener(OnEndCutscene);
         cutscene.transform.position = new Vector3();
+    }
+
+    public void OnEndCutscene(Transform t) {
+        var blackHole = Instantiate(BlackHolePrefab);
+        blackHole.transform.position = BlackHoleSpawnPoint.position;
+        blackHole.PathMarkers = PathMarkers;
+        ForegroundCamera.brownHole = blackHole.transform;
         Destroy(gameObject);
     }
 }
